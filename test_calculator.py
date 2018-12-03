e57f4be0fe6b4e26a55320ddf93fbdbbe339750e
Nicolas Segoviano <nicolas.segoviano@gmail.com>
2018-12-03 13:56:13 -0800

    Add unit test for calculator

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+"""
+Unit test for calculator library.
+"""
+
+import calculator
+
+
+
+class TestCalculator:
+    
+    def test_adition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_substraction(self):
+        assert 2 == calculator.subtract(4, 2)
+
+
