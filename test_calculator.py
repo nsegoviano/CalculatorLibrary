e005bf2dfbe2947b9476149d788eee0bc1363680
Nicolas Segoviano <nicolas.segoviano@gmail.com>
2019-01-31 14:45:02 -0800

    Update main file

--- test_calculator.py
+++ test_calculator.py
@@ -7,11 +7,10 @@ import calculator
 
 
 class TestCalculator:
-    
+
     def test_adition(self):
         assert 4 == calculator.add(2, 2)
 
     def test_substraction(self):
         assert 2 == calculator.subtract(4, 2)
-
 
