b4b1e1dce8db0030389cd6e29ee5237ef4364fe6
Nicolas Segoviano <nicolas.segoviano@gmail.com>
2019-02-04 18:56:38 -0800

    More fixes.

--- test_factorial.py
+++ test_factorial.py
@@ -2,10 +2,9 @@
 Unit test for factorial calculator library.
 """
 import unittest
-from  factorial_calculator import factorial
+from factorial_calculator import factorial
 
 class TestFactorialCalculator(unittest.TestCase):
-
 
     def test_factorial(self):
         res = factorial(0)
